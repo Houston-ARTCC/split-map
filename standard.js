c319e35e418351c3ecad948210c05a9908df0a7f
Dave Wegner <44472479+jivebiscuit@users.noreply.github.com>
2025-06-22 21:34:24 -0500

    Update standard.js

--- standard.js
+++ standard.js
@@ -38,13 +38,13 @@
 //     L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
 // Define various colors used on map
-    var color1 = '#264653'; // current=RSG // new=RSG
-    var color6 = '#287271'; // current=AUS // new=LCH
-    var color5 = '#2A9D8F'; // current=LFK // new=OCN
-    var color3 = '#8AB17D'; // current=NEW // new=LFK
-    var color7 = '#C6A75A'; // current=OCN // new=CRP
-    var color4 = '#F4A261'; // current=LCH // new=NEW
-    var color2 = '#E76F51'; // current=CRP // new=AUS
+    var color1 = '#264653'; // old=RSG // new=RSG
+    var color2 = '#E76F51'; // old=CRP // new=AUS
+    var color3 = '#8AB17D'; // old=NEW // new=LFK
+    var color4 = '#F4A261'; // old=LCH // new=NEW
+    var color5 = '#2A9D8F'; // old=LFK // new=OCN
+    var color6 = '#287271'; // old=AUS // new=LCH
+    var color7 = '#C6A75A'; // old=OCN // new=CRP
     var colorDefault = 'gray';
 
 // Define your GeoJSON data directly as a JavaScript object
